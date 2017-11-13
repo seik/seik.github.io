ba0254883d93cac77ca5f2359cfca3bc46c1425c
Iván Martín <ivanmartinga@gmail.com>
2017-11-13 16:50:44 +0100

    Js clicks

--- animation.js
+++ animation.js
@@ -7,12 +7,20 @@ $( document ).ready(function() {
 		$("#twitterbg").stop().hide("2000")
 	});
 
+	$("#twitter").click(function() {
+		document.location = "https://twitter.com/seikv";		
+	} );
+
 	$("#github").stop().mouseenter(function(){
 		$("#githubbg").stop().show("2000")
 	});
 
 	$("#github").stop().mouseleave(function(){
 		$("#githubbg").stop().hide("2000")
+	});
+
+	$("#github").click(function() {
+		document.location = "https://github.com/seik";		
 	});
 
 	$("#telegram").stop().mouseenter(function(){
@@ -23,11 +31,19 @@ $( document ).ready(function() {
 		$("#telegrambg").stop().hide("2000")
 	});
 
+	$("#telegram").click(function() {
+		document.location = "http://telegram.me/seikv";		
+	});
+
 	$("#email").stop().mouseenter(function(){
 		$("#emailbg").stop().show("2000")
 	});
 
 	$("#email").stop().mouseleave(function(){
 		$("#emailbg").stop().hide("2000")
+	});
+
+	$("#email").click(function() {
+		document.location = "mailto:ivanmartinga@gmail.com";		
 	});
 });
