2f37d07379561fc6a39eee9c5b5d33933b77b696
Iván Martín <ivanmartinga@gmail.com>
2017-11-13 16:27:37 +0100

    Use static images and add email

--- animation.js
+++ animation.js
@@ -2,28 +2,32 @@ $( document ).ready(function() {
 	$("#twitter").stop().mouseenter(function(){
 		$("#twitterbg").stop().show("2000")
 	});
-	$("#twitter").click(function() { 
-		document.location = "https://twitter.com/seikv";
-	} );
+
 	$("#twitter").stop().mouseleave(function(){
 		$("#twitterbg").stop().hide("2000")
 	});
+
 	$("#github").stop().mouseenter(function(){
 		$("#githubbg").stop().show("2000")
 	});
-	$("#github").click(function() { 
-		document.location = "https://github.com/seik";
-	} );
+
 	$("#github").stop().mouseleave(function(){
 		$("#githubbg").stop().hide("2000")
 	});
+
 	$("#telegram").stop().mouseenter(function(){
 		$("#telegrambg").stop().show("2000")
 	});
-	$("#telegram").click(function() { 
-		document.location = "http://telegram.me/seikv";
-	} );
+
 	$("#telegram").stop().mouseleave(function(){
 		$("#telegrambg").stop().hide("2000")
+	});
+
+	$("#email").stop().mouseenter(function(){
+		$("#emailbg").stop().show("2000")
+	});
+
+	$("#email").stop().mouseleave(function(){
+		$("#emailbg").stop().hide("2000")
 	});
 });
